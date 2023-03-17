4151e4c15cfdf2b09a77eb83fc6487ff1a3ca4a9
Moses <mosemuthoni57@gmail.com>
2023-03-17 05:01:12 -0700

    Using stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  *main - description of a main function
- *Return 0: (Success)
+ *Return: 0 (Success)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, " \"and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n");
+	return (0);
 }
