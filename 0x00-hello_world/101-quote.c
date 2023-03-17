a3cd2ce26e7980168e66612e7b345e6e6dbfa8bf
Moses <mosemuthoni57@gmail.com>
2023-03-17 04:52:44 -0700

    Using the Stderr output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main - description of a main function
+ *Return 0: (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+}
