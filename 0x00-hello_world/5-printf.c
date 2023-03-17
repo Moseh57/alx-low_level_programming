3b5b860747bb38abfcd98e74769958f05de99df1
Moses <mosemuthoni57@gmail.com>
2023-03-16 19:45:01 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a function that prints
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle\n");
+return (0);
+
