f4c89d9fd4d6ba06910d9fc87647ef5778748ea8
Moses <mosemuthoni57@gmail.com>
2023-03-19 01:06:47 -0700

    Printing combination of single numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a function that prints
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10)
+	{
+		putchar(a + '0');
+		putchar(',');
+		putchar('\n');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
