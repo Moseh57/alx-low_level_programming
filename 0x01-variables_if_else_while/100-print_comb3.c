8d820d9f912b63c7b2e900a51f9f07408f0c6ef4
Moses <mosemuthoni57@gmail.com>
2023-03-19 01:43:25 -0700

    Printing combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - a function that prints
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10;)
+	{
+		for (b = 0; b < 10;)
+		{
+			if (a != b)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
