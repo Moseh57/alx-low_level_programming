0b4c420fafb5d113187d31caa182bcba478330f4
Moses <mosemuthoni57@gmail.com>
2023-03-19 01:12:38 -0700

    Printing combination of single numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 10;)
+	for (a = 0; a < 9;)
 	{
 		putchar(a + '0');
 		putchar(',');
-		putchar('\n');
 		a++;
 	}
+	putchar(9 + '0');
 	putchar('\n');
 	return (0);
 }
