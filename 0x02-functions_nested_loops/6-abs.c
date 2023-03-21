dfc79c31909ea7d2ef26daf0096541fc1abb447b
Moses <mosemuthoni57@gmail.com>
2023-03-21 14:40:24 -0700

    Find the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - checks for lowercase
+ * @a: integer to be checked
+ * Return: the absolute value
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = a * -1;
+	}
+	return (a);
+}
