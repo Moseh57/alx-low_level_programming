5dbb3b4efd70d78849d279bf9ca11e42df2370f6
Moses <mosemuthoni57@gmail.com>
2023-03-21 22:14:04 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table - checks for lowercase
+ * Return: the absolute value
+ */
+
+void times_table(void)
+{
+	int a, b, c, d, e;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = (b * a);
+			if (c > 9)
+			{
+				d = (c % 10);
+				e = (c - d) / 10;
+				_putchar(e + '0');
+				_putchar(d + '0');
+			}
+			else
+				_putchar(c + '0');
+			if (b == 9)
+			{
+				_putchar('$');
+				_putchar('\n');
+				break;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+}
