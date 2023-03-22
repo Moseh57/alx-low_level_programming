1ce2385e54a1ff954b52dc753779d180446da41e
Moses <mosemuthoni57@gmail.com>
2023-03-22 12:57:10 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - function for fibonnaci
+ * Return: 0 (Success)
+ */
+
+
+intt main(void)
+{
+	int a;
+	unsigned long int f;
+	unsigned long int f2;
+	unsigned long int b;
+
+	f = 0;
+	f2 = 1;
+
+	for (a = 0; a < 50; a++)
+	{
+		b = f + f2;
+		printf("%lu", b);
+
+		f = f2;
+		f2 = b;
+
+		if (a == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
