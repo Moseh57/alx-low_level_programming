4578ca42fbdf1ba455a97e082872fe290672a878
Moses <mosemuthoni57@gmail.com>
2023-03-21 22:53:15 -0700

    Printing the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - checks for lowercase
+ * @a: integer to add
+ * @b: integer to add
+ * Return: the sum
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = (a + b);
+	return (result);
+}
