3ff0f591ce2e9a8c815d846eed51d4b494aaa368
Moses <mosemuthoni57@gmail.com>
2023-03-27 08:16:08 -0700

    Printing in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - a function to print in reverse order
+ * @s: a character to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int k;
+	int i;
+
+	i = strlen(s);
+	for (k = i; k >= 0; k--)
+	{
+		_putchar(s[k]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,5 @@ void reset_to_98(int *n);
 void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *);
-
+void print_rev(char *s);
 #endif
