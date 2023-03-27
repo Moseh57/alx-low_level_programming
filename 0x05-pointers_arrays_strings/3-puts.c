0dca2811b90471137c3c6ceb64244476c8366fba
Moses <mosemuthoni57@gmail.com>
2023-03-27 07:55:13 -0700

    Printing usong pus

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - a function for printing a srting
+ * @str: a character to be checked.
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,6 @@ int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *, int *);
 int _strlen(char *);
+void _puts(char *);
+
 #endif
