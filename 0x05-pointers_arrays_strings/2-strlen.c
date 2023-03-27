63642ea17531ab2d6bd5911bbe8149703b8a9184
Moses <mosemuthoni57@gmail.com>
2023-03-27 07:33:30 -0700

    checking the length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: a character to check the length
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *, int *);
+int _strlen(char *);
 #endif
