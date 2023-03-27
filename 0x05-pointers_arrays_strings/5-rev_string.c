682e6d3f4131eb5f48322a0f74eef7945d30a9e2
Moses <mosemuthoni57@gmail.com>
2023-03-27 09:21:46 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include "main.h"
+/**
+ * rev_string - a function to print in reverse order
+ * @s: a character to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int k;
+	int i;
+	int p;
+	char temp[10];
+
+	p = 0;
+	i = (strlen(s) - 1);
+	for (k = i; k >= 0; k--)
+	{
+		if (p <= 9)
+		{
+			temp[p] = s[k];
+			p++;
+		}
+	}
+	for (k = 0; k <= i; k++)
+	{
+		s[k] = temp[k];
+	}
+}
