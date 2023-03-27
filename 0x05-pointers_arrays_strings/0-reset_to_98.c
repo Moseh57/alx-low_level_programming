823ad85f46d03611e94a810b76fa7a73c1f005ac
Moses <mosemuthoni57@gmail.com>
2023-03-27 07:01:55 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 -  afunction to change value
+ * @n: apointer to n
+ * Return: null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this folder am learning about arrays pointers and strings
