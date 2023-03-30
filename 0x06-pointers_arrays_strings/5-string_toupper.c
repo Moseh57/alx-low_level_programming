e35bc03846a6ae1f749631581ec25cc9514d4e7c
Moses <mosemuthoni57@gmail.com>
2023-03-30 13:51:10 -0700

    Upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * String_toupper - a function that capitalizes 
+ * @a: the string
+ * Return: the uppercase string
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
+	}
+	return (a);
+}
