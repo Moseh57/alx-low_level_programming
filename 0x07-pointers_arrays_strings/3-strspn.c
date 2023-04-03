132331a99dc9cebbbfbefce72ca6775b84f73b97
Moses <mosemuthoni57@gmail.com>
2023-04-03 08:51:17 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int k = 0;
+	int h, z = 0;
+	unsigned int f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		k++;
+	if (i == 0 && s[i] == '\0')
+		k++;
+	for (h = 0; accept[h] != '\0'; h++)
+		z++;
+	if (h == 0 && accept[h] == '\0')
+		z++;
+	for (i = 0; i <= z; i++)
+	{
+		for (j = 0; j <= k; j++)
+		{
+		if (accept[i] == s[j])
+		{
+			f = f + 1;
+			break;
+		}
+		}
+	}
+	return (f);
+}
