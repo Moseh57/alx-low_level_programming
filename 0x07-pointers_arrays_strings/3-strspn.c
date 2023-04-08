f1624f5fa551a71313470f04cfbc46fda85db2ff
Moses <mosemuthoni57@gmail.com>
2023-04-07 23:41:38 -0700

    count bytes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,27 +7,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	int k = 0;
-	int h, z = 0;
-	unsigned int f = 0;
+	unsigned int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
-		k++;
-	for (h = 0; accept[h] != '\0'; h++)
-		z++;
-	for (i = 0; i < (z + 1); i++)
+	for (i = 0; s[m]; i++)
 	{
-		for (j = 0; j < (k + 1); j++)
+		for (j = 0; accept[j]; j++)
 		{
 		if (accept[i] == s[j])
 		{
-			f = f + 1;
 			break;
 		}
-		else
-			continue;
+		
 		}
+		if (accept[j] == '\0')
+			return (i);
 	}
-	return (f);
+	return (i);
 }
