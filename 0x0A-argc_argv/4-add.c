a76d17402608b6359a8a0504a424a673748476ea
Moses <mosemuthoni57@gmail.com>
2023-04-17 13:19:18 -0700

    agrv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main  - a function that adds args
+ * @argc: integer to store arg length
+ * @argv: array of arg
+ * Return: zero on success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc <= 2)
+		printf("0\n");
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (isdigit(atoi(argv[i])) != 1)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+		return (0);
+}
