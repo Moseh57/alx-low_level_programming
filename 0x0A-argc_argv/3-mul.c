e9d43715820b81226443fffa82af7896d265d92a
Moses <mosemuthoni57@gmail.com>
2023-04-17 12:38:06 -0700

    print product

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
+ * main - a function that prints the product of two integers
+ * @argc: integer to store the no of arguments
+ * @argv: array for the number of arguments
+ * Return: zero if success
  */
 int main(int argc, char *argv[])
 {
