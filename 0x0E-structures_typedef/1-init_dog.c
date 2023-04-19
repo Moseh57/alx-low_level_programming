c018b39c186d13d2c0ba4cbee2d4b9ae71674678
Moses <mosemuthoni57@gmail.com>
2023-04-19 05:53:37 -0700

    create a variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - a function that creates a variable of type struct dog
+ * @name: the odgs name
+ * @age: the dogs age
+ * @owner: the dogs owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
