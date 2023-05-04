af90a1ca65d006d5af8cabb93588b2ee60ea79be
Moses <mosemuthoni57@gmail.com>
2023-05-04 15:33:27 -0700

    Bit Manupulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - a function that prints binary form of a number
+ * @n: the decimal number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int i, j = 0;
+
+	if (n == 0)
+		_putchar(0 + '0');
+	for (i = 63; i >= 0; i--)
+	{
+		temp = n >> i;
+		if ((temp & 1) == 1)
+		{
+			_putchar('0' + 1);
+			j++;
+		}
+		else if (j != 0)
+			_putchar(0 + '0');
+	}
+	_putchar('\n');
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char);
+void print_binary(unsigned long int n);
+#endif
