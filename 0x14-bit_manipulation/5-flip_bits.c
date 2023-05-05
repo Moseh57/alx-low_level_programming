0612424d5be4d4fc46a9ef2da65a8614038545aa
Moses <mosemuthoni57@gmail.com>
2023-05-05 04:53:28 -0700

    Flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * flip_bits - a function that checks flips to be performed
+ * @n: the first integer to look
+ * @m: the second integer to look
+ * Return: the number of bits to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i;
 	unsigned int j = 0;
-	unsigned int temp, exor;
+	unsigned long int temp, exor;
 
 	exor = n ^ m;
-	for (i = 0; i < 12; i++)
+	for (i = 0; i < 32; i++)
 	{
 		temp = exor & (1 << i);
 		if (temp > 0)
