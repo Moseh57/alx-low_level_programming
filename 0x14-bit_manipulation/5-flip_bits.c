52354e7de2aab90afb87b09df76707720856b0a4
Moses <mosemuthoni57@gmail.com>
2023-05-05 04:37:42 -0700

    Flip Bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *
  *
@@ -6,7 +7,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, j = 0;
+	int i;
+	unsigned int j = 0;
 	unsigned int temp, exor;
 
+	exor = n ^ m;
+	for (i = 0; i < 12; i++)
+	{
+		temp = exor & (1 << i);
+		if (temp > 0)
+			j++;
+	}
+	return (j);
 }
