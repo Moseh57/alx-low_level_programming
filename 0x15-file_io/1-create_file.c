cf7f45131e1c69b998eead41a3d0ecc4a61846be
Moses <mosemuthoni57@gmail.com>
2023-05-09 15:47:09 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,17 +21,22 @@ int counts(char *text)
  */
 int create_file(const char *filename, char *text_content)
 {
+	int a;
 	ssize_t fd;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
-	write(fd, text_content, counts(text_content));
+	a = write(fd, text_content, counts(text_content));
+	if (a == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
