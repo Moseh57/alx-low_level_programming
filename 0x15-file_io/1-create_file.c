b04d3134800e509f89cb69a34db3df876b65c7ac
Moses <mosemuthoni57@gmail.com>
2023-05-09 15:42:27 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * counts - a function that helps me count length of string
+ * @text: the string ti count
+ * Return: the length of the string
+ */
+int counts(char *text)
+{
+	int i = 0;
+
+	while (text[i] != '\0')
+		i++;
+	i += 1;
+	return (i);
+}
+/**
+ * create_file - a function that creates a file
+ * @filename: the name of the file to create
+ * @text_content: the content of the file
+ * Return: 1 if success -1 if error
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	write(fd, text_content, counts(text_content));
+	close(fd);
+	return (1);
+}
