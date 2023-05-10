4d5ae631490c1d4c21c54bff3eadd30ea9ff71bf
Moses <mosemuthoni57@gmail.com>
2023-05-09 18:57:30 -0700

    copy file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int ac, char **av)
 {
-	int fd, fd2, n;
+	int fd, fd2, n, w;
 	char buff[1024];
 
 	if (ac != 3)
@@ -18,13 +18,18 @@ int main(int ac, char **av)
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file%s", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((n = read(fd, buff, 1024)) > 0)
 	{
-		write(fd2, buff, n);
+		w = write(fd2, buff, n);
+		if (w == -1 || fd2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
 	}
 	close(fd);
 	close(fd2);
